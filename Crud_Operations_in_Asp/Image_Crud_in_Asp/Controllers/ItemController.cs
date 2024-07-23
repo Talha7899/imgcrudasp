895678154df9fbb19235e8c8f497ce1ebaf41eb6
asp <asp@aptech.com>
2024-07-23 11:47:18 +0500

    first commit

---
+++ Crud_Operations_in_Asp/Image_Crud_in_Asp/Controllers/ItemController.cs
@@ -0,0 +1,53 @@
+﻿using Image_Crud_in_Asp.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Image_Crud_in_Asp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ImgCrudContext db;
+
+        public ItemController(ImgCrudContext _db)
+        {
+            this.db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            var itemData = db.Items.Include(x => x.Cat);
+            return View(itemData.ToList());
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            ViewBag.CatId = new SelectList(db.Categories,"Catid","CatName");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item, IFormFile file)
+        {
+            string imageName = DateTime.Now.ToString("yymmddhhmmss");
+            imageName += Path.GetFileName(file.FileName);
+            var imagePath = Path.Combine(HttpContext.Request.PathBase.Value,"wwwroot/uploads");
+            var imageValue = Path.Combine(imagePath,imageName);
+
+            using(var stream = new FileStream(imageValue,FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            var dbimage = Path.Combine("/uploads", imageName);
+            item.Image = dbimage;
+            db.Items.Add(item);
+            db.SaveChanges();
+            ViewBag.CatId = new SelectList(db.Categories, "Catid", "CatName");
+            return RedirectToAction("Index");
+        }
+    }
+}
